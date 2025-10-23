da1e9d3d35bba2e4d4b776fc34ebfc018cf72d6d
wenkai-li <133668540+wenkai-li@users.noreply.github.com>
2025-10-23 10:04:46 +0000

    Deploying to gh-pages from @ wenkai-li/wenkai-li.github.io@76b1fec4a81b9f6c93b7bb7ee028aac8d38365a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "cv",
           description: "",
           section: "Navigation",
